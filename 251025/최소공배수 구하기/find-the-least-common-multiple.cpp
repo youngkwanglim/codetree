eeaf09b0de1c482b0a18d6642bb75e1997614e62
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2025-10-25 14:20:38 +0900

    Title: 최소공배수 구하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251025/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 251025/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -7,9 +7,10 @@ int main() {
     cin >> n >> m;
 
     // Please write your code here.
-    for(int i = 100; i > 0; i--){
+    for(int i = 1; i <= 100; i++){
         if(i % n == 0 && i % m == 0){
             ret = i;
+            break;
         }
     }
     cout << ret;
