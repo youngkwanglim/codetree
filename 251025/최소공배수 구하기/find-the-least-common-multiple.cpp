9fe295e45e8c14cfe971287b343d7d1ee4e33741
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2025-10-25 14:25:52 +0900

    Title: 최소공배수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251025/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 251025/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -3,17 +3,21 @@ using namespace std;
 
 int n, m, ret = 1;
 
+void go(int n, int m){
+    int gcd = 0;
+    for(int i = 1; i <= min(n,m); i++){
+        if(n % i == 0 && m % i == 0){
+            gcd = i;
+        }
+    }
+    cout << n * m / gcd;
+}
+
 int main() {
     cin >> n >> m;
 
     // Please write your code here.
-    for(int i = 1; i <= 10000000; i++){
-        if(i % n == 0 && i % m == 0){
-            ret = i;
-            break;
-        }
-    }
-    cout << ret;
+    go(n,m);
 
     return 0;
 }
