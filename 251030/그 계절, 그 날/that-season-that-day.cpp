bca1ab5618e0985f72fcbbec2504005ce757907d
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2025-10-30 00:36:30 +0900

    Title: 그 계절, 그 날, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251030/그 계절, 그 날/that-season-that-day.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int Y, M, D;
+
+bool checkYoon(int Y){
+    if(Y % 400) return true;
+    else if(Y % 100) return false;
+    else if(Y % 4) return true;
+    return false;
+}
+
+int main() {
+    cin >> Y >> M >> D;
+
+    // Please write your code here.
+    // 월을 비교해서 체크해야될듯 월별로 달이 나눠져있음
+    if(M >= 3 && M <= 5){
+        if(M == 4 && D == 31) {
+            cout << -1;
+        }
+        else{
+            cout << "Spring";
+        }
+    }
+    else if(M >= 6 && M <= 8){
+        if(M == 6 && D == 31) {
+            cout << -1;
+        }
+        else{
+            cout << "Summer";
+        }
+    }
+    else if(M >= 9 && M <= 11){
+        if((M == 9 || M == 11) && D == 31) {
+            cout << -1;
+        }
+        else{
+            cout << "Fall";
+        }        
+    }
+    else{
+        if(M == 2 && checkYoon(Y) && D >= 30){
+            cout << -1;
+        }
+        else if(M == 2 && !checkYoon(Y) && D >= 29){
+            cout << -1;
+        }
+        else{
+            cout << "Winter";
+        }
+    }
+    return 0;
+}
