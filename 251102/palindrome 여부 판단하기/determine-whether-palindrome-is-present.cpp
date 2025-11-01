187d8cba1019eab384ba10244933f6ba2435d629
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2025-11-02 00:19:11 +0900

    Title: palindrome 여부 판단하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251102/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string A;
+bool go(string s){
+    for(int i = 0; i <= s.size() / 2; i++){
+        if(s[i] == s[s.size() - i - 1]){
+            //cout << i << " ";
+            continue;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    cin >> A;
+
+    // Please write your code here.
+    if(go(A) == true) cout << "Yes";
+    else cout << "No";
+
+    return 0;
+}
