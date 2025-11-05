b418be96b95f09356fdb61f0825faeecfa61fb54
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2025-11-06 01:31:55 +0900

    Title: 단어 정렬, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251106/단어 정렬/sorting-words.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n;
+string word[100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> word[i];
+    }
+
+    // Please write your code here.
+    sort(word, word + n);
+    for (int i = 0; i < n; i++) {
+        cout << word[i] << '\n';
+    }
+
+    return 0;
+}
