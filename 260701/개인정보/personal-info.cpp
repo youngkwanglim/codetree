ecd978f7fcffeccc03c63613454cec4df5f007d7
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2026-07-01 23:30:38 +0900

    [Passed] 개인정보 (2ms, 0MB)

---
+++ 260701/개인정보/personal-info.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+const int n = 5;
+
+struct Student{
+    string name;
+    int h;
+    float w;
+};
+
+bool cmp(Student A, Student B){
+    return A.h > B.h;
+}
+
+bool cmp2(Student A, Student B){
+    return A.name < B.name;
+}
+
+int main() {
+    // Please write your code here.
+    Student students[n];
+    cout << fixed << setprecision(1);
+
+    for(int i = 0; i < n; i++){
+        cin >> students[i].name >> students[i].h >> students[i].w;
+    }
+
+    //sort(students, students + n);
+    sort(students, students + n, cmp2);
+    cout << "name" << '\n';
+    for(int i = 0; i < n; i++){
+        cout << students[i].name << " " << students[i].h << " " << students[i].w << '\n';
+    }
+    cout << '\n';
+
+    sort(students, students + n, cmp);
+    cout << "height" << '\n';
+    for(int i = 0; i < n; i++){
+        cout << students[i].name << " " << students[i].h << " " << students[i].w << '\n';
+    }
+    return 0;
+}
