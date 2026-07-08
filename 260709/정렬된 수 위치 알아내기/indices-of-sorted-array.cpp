351d196a77e2cd2d4194e932992c4172f808c829
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2026-07-09 02:07:09 +0900

    [Passed] 정렬된 수 위치 알아내기 (2ms, 0MB)

--- 260709/정렬된 수 위치 알아내기/indices-of-sorted-array.cpp
+++ 260709/정렬된 수 위치 알아내기/indices-of-sorted-array.cpp
@@ -1,50 +1,31 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int n;
-pair<int, int> p;
-
-struct Point{
-    int input, start, end;
-};
-
-bool cmp(Point A, Point B){
-    if(A.input == B.input) return A.start < B.start;
-    return A.input < B.input;
-}
 
 int main() {
     // Please write your code here.
+     // 두번째 필드에 원래 위치를 저장한다. 그리고 정렬하고 새로운 배열에 그 값들을 넣어준다.
     cin >> n;
-    Point p[n];
+    vector<pair<int, int>> p(n);
     for(int i = 0; i < n; i++){
-        cin >> p[i].input;
-        p[i].start = i + 1;
+        cin >> p[i].first;
+        p[i].second = i + 1;
     }
 
-    sort(p, p + n, cmp);
+    sort(p.begin(), p.end());
 
-    // for(int i = 0; i < n; i++){
-    //     p[i].end = i + 1;
-    // }
-
-    // cout << p[p[i].input;
 
     int ret[n + 4];
-    // for(int i = 0; i < n; i++){
-    //     p[i].end = i + 1;
-    // }
 
     for(int i = 0; i < n; i++){
-        ret[p[i].start] = i + 1;
+        ret[p[i].second] = i + 1;
     }
 
     for(int i = 0; i < n; i++){
         cout << ret[i + 1] << " ";
     }
-
-    // 두번째 필드에 원래 위치를 저장한다. 그리고 정렬하고 새로운 배열에 그 값들을 넣어준다.
-
     return 0;
 }
