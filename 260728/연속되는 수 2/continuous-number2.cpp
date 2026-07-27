4d6042adf08c963a82a7cf52ce10e45b833ab577
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2026-07-28 08:05:30 +0900

    [Passed] 연속되는 수 2 (2ms, 0MB)

--- 260728/연속되는 수 2/continuous-number2.cpp
+++ 260728/연속되는 수 2/continuous-number2.cpp
@@ -9,7 +9,7 @@ int main() {
         cin >> a[i]; 
     }
 
-    int tmp = 0;
+    int tmp = 1;
     for(int i = 0; i < n; i++){
         if(i != 0 && a[i] == a[i - 1]) tmp++;
         else tmp = 1;
