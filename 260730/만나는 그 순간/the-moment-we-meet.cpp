57436167e67fcc4f913043bb5432d5479beebe3a
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2026-07-30 07:25:09 +0900

    [Passed] 만나는 그 순간 (1ms, 0MB)

---
+++ 260730/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+int n, m, a[1000004], b[1000004], k, cnt = 1, now;
+char c;
+
+int main() {
+    // Please write your code here.
+    cin >> n >> m;
+    now = 0;
+    cnt = 1;
+    for(int i = 0; i < n; i++){
+        cin >> c >> k;
+        //cnt++;
+        if(c == 'R'){
+            while(k--){
+                a[cnt] = ++now;
+                // cout << cnt << "에 " << now << '\n';
+                //now++;
+                cnt++;
+            }
+        }
+        else{
+            while(k--){
+                a[cnt] = --now;
+                // cout << cnt << "에 " << now << '\n';
+                //now--;
+                cnt++;
+            }
+        }
+    }
+
+    now = 0;
+    cnt = 1;
+    for(int i = 0; i < m; i++){
+        cin >> c >> k;
+        //cnt++;
+        if(c == 'R'){
+            while(k--){
+                b[cnt] = ++now;
+                // cout << cnt << "에 " << now << '\n';
+                //now++;
+                cnt++;
+            }
+        }
+        else{
+            while(k--){
+                b[cnt] = --now;
+                // cout << cnt << "에 " << now << '\n';
+                //now--;
+                cnt++;
+            }
+        }
+    }
+
+    for(int i = 1; i <= cnt - 1; i++){
+        if(a[i] == b[i]) {
+            //cout << a[i] << " " << b[i] << '\n';
+            cout << i;
+            return 0;
+        }
+    }
+
+    cout << -1;
+    return 0;
+}
