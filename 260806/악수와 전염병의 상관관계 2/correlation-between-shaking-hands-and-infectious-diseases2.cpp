ebbde8483aac91e522d77d8f6b15cccc8ee97dcf
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2026-08-06 08:58:14 +0900

    [Passed] 악수와 전염병의 상관관계 2 (2ms, 0MB)

--- 260806/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
+++ 260806/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -1,31 +1,63 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-int n, k, p, t, a[104], cnt[104];
-pair<int, int> gam[255];
+int n, k, p, t;
+int a[104], cnt[104];
+
+struct Shake {
+    int time, x, y;
+};
+
+Shake shake[254];
+
+bool cmp(Shake a, Shake b){
+    return a.time < b.time;
+}
 
 int main() {
+    // Please write your code here.
     cin >> n >> k >> p >> t;
     a[p] = 1;
 
-    for (int i = 0; i < t; i++) {
-        int T, x, y;
-        cin >> T >> x >> y;
-        gam[T] = {x, y};   // 시간을 인덱스로 → 정렬 불필요
+    for(int i = 0; i < t; i++){
+        // 헌명이 감염되어 있으면, 그 반대편이 감염됨
+        // 대신 k번까지만 감염시킴.
+        cin >> shake[i].time >> shake[i].x >> shake[i].y;     
     }
 
-    for (int i = 1; i <= 250; i++) {
-        int x = gam[i].first, y = gam[i].second;
-        if (x == 0) continue;          // 그 시간에 악수 없음
+    sort(shake, shake + t, cmp);
+    for(int i = 0; i < t; i++){
+        int x = shake[i].x, y = shake[i].y;
+        if(x == 0) continue;
 
-        bool ix = a[x], iy = a[y];     // 이 악수 '전'의 감염 상태를 먼저 저장
+        bool ix = a[x], iy = a[y]; // 감염상태를 저장
 
-        if (ix) cnt[x]++;              // 감염자는 무조건 악수 횟수 소모
-        if (iy) cnt[y]++;
+        if(ix) cnt[x]++;
+        if(iy) cnt[y]++;
 
-        if (ix && cnt[x] <= k) a[y] = 1;
-        if (iy && cnt[y] <= k) a[x] = 1;
+        if(ix && cnt[x] <= k) a[y] = 1;
+        if(iy && cnt[y] <= k) a[x] = 1;
+
+        // if(a[gam[i].first] > 0 && cnt[gam[i].first] < k){
+        //     cnt[gam[i].first]++;
+        //     a[gam[i].second]++;
+        //     a[gam[i].second]++;
+        //     //cout << i << "초에 감염\n";
+        //     //cout << gam[i].first << '\n';
+        //     //cout << gam[i].second << '\n';
+        // }
+        // else if(a[gam[i].second] > 0 && cnt[gam[i].second] < k){
+        //     cnt[gam[i].second]++;
+        //     a[gam[i].first]++;
+        //     a[gam[i].second]++;
+        //     //cout << i << "초에 감염\n";
+        // }
     }
 
-    for (int i = 1; i <= n; i++) cout << (a[i] ? 1 : 0);
+    for(int i = 1; i <=n; i++){
+        if(a[i]) cout << 1;
+        else cout << 0;
+    }
+
     return 0;
-}
+};
