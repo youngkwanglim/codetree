3423b703b3cf7f888cdb7924466765e23da8ff69
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2026-08-13 08:41:52 +0900

    [Passed] 체크판위에서 2 (2ms, 0MB)

--- 260813/체크판위에서 2/on-the-checkboard-2.cpp
+++ 260813/체크판위에서 2/on-the-checkboard-2.cpp
@@ -13,8 +13,8 @@ int main() {
     }
 
     // 모든 위치 2개를 조합해서 첫번째랑 두번째랑 세번째가 각각 색깔 다른지 체크하면 될듯함
-    for(int i = 1; i < r - 2; i++){
-        for(int j = 1; j < c - 2; j++){
+    for(int i = 1; i < r - 1; i++){
+        for(int j = 1; j < c - 1; j++){
             for(int k = i + 1; k < r - 1; k++){
                 for(int l = j + 1; l < c - 1; l++){
                     if(a[0][0] != a[i][j] && a[i][j] != a[k][l] && a[k][l] != a[r - 1][c - 1]) ret++;
