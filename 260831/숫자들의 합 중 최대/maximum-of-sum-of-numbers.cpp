84b9cb93e5cddeac225b11a6db9f501222751009
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2026-08-31 07:27:53 +0900

    [Passed] 숫자들의 합 중 최대 (2ms, 0MB)

--- 260831/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
+++ 260831/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
@@ -2,17 +2,16 @@
 using namespace std;
 int x, y, ret;
 
+int DigitSum(int n){
+    if(n < 10) return n;
+    else return DigitSum(n / 10) + (n % 10);
+}
+
 int main() {
     // Please write your code here.
     cin >> x >> y;
     for(int i = x; i <= y; i++){
-        int tmp = 0;
-        int now = i;
-        while(now > 0){
-            tmp += now % 10;
-            now /= 10;
-        }
-        ret = max(ret, tmp);
+        ret = max(ret, DigitSum(i));
     }
     cout << ret;
     return 0;
