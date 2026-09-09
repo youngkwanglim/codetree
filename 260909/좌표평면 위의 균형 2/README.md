a94a7af8cf934f33cba331da91d9cee82ccc7aa7
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2026-09-09 19:45:10 +0900

    [Passed] 좌표평면 위의 균형 2 (2ms, 0MB)

---
+++ 260909/좌표평면 위의 균형 2/README.md
@@ -0,0 +1,8 @@
+# [[챌린지]좌표평면 위의 균형 2](https://www.codetree.ai/trails/complete/curated-cards/challenge-balance-on-coordinate-plane-2)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 2 / 완전탐색 III / 상황을 일일이 가정해보고 진행하는 완전탐색](https://www.codetree.ai/trail-info/novice-mid/) |
+| 난이도 | 보통 |
+| 경험치 | 50 XP |
---
+++ 260909/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+using namespace std;
+int n, x[104], y[104], ret = INT_MAX;
+
+int main() {
+    // Please write your code here.
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> x[i] >> y[i];
+    }
+
+    for(int i = 2; i <= 100; i += 2){
+        for(int j = 2; j <= 100; j += 2){
+            int cnt_1 = 0;
+            int cnt_2 = 0;
+            int cnt_3 = 0;
+            int cnt_4 = 0;
+
+            for(int k = 0; k < n; k++){
+                if(x[k] > i && y[k] > j) cnt_1++;
+                if(x[k] < i && y[k] > j) cnt_2++;
+                if(x[k] > i && y[k] < j) cnt_3++;
+                if(x[k] < i && y[k] < j) cnt_4++;
+            }
+            ret = min(ret, max(max(cnt_1, cnt_2), max(cnt_3, cnt_4)));
+        }
+    } 
+    cout << ret;
+    return 0;
+}
