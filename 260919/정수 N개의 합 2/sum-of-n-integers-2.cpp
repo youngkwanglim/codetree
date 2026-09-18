7f35f8d62b2fc9dd6c49ea57350d3cc4dc894dc7
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2026-09-19 02:52:06 +0900

    [Passed] 정수 N개의 합 2 (25ms, 1MB)

---
+++ 260919/정수 N개의 합 2/sum-of-n-integers-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int n, k;
+int a[100004], psum[100004], ret = -INT_MAX;
+
+int main() {
+    // Please write your code here.
+    cin >> n >> k;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    for(int i = 1; i <= n; i++){    // 1부터 n까지 차있음.
+        psum[i] = psum[i - 1] + a[i - 1];
+        //cout << psum[i] << ' ';
+    }
+
+    for(int i = 1; i <= n - k - 1; i++){
+        ret = max(ret, psum[i + k] - psum[i]);
+    }
+    cout << ret;
+    return 0;
+}
