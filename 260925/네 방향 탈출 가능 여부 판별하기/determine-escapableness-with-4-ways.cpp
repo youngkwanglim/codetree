d2cb642f0f7dbdc5969e53e1aa8d4279f0e19fcc
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2026-09-25 02:35:12 +0900

    [Passed] 네 방향 탈출 가능 여부 판별하기 (2ms, 0MB)

--- 260925/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
+++ 260925/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -15,7 +15,7 @@ bool CanGo(int y, int x){
     return true;
 }
 
-void DFS(int y, int x){
+void BFS(int y, int x){
     while(q.size()){
         tie(y, x) = q.front();
         q.pop();
@@ -42,7 +42,7 @@ int main() {
 
     visited[0][0] = 1;
     q.push({0, 0});
-    DFS(0, 0);
+    BFS(0, 0);
 
     if(visited[n - 1][m - 1]) cout << 1;
     else cout << 0;
