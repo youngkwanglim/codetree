c874415e29f2cb23a69bf2733b814fceb7ebfebb
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2026-09-25 02:33:18 +0900

    [Passed] 네 방향 탈출 가능 여부 판별하기 (2ms, 0MB)

---
+++ 260925/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int n, m, a[104][104], visited[104][104];
+int dy[4] = {1, 0, -1, 0}, dx[4] = {0, -1, 0, 1}, y, x;
+queue<pair<int , int>> q;
+
+bool IsIn(int y, int x){
+    return 0 <= y && y < n && 0 <= x && x < m;
+}
+
+bool CanGo(int y, int x){
+    if(!IsIn(y, x)) return false;
+    if(visited[y][x] || a[y][x] == 0) return false;
+    return true;
+}
+
+void DFS(int y, int x){
+    while(q.size()){
+        tie(y, x) = q.front();
+        q.pop();
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(CanGo(ny, nx)){
+                visited[ny][nx] = 1;
+                q.push({ny, nx});
+            }
+        }
+    }
+
+}
+
+int main() {
+    // Please write your code here.
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    visited[0][0] = 1;
+    q.push({0, 0});
+    DFS(0, 0);
+
+    if(visited[n - 1][m - 1]) cout << 1;
+    else cout << 0;
+    return 0;
+}
